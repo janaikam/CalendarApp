248ae94c3296b1d68b111b6ba82b9b44735be749
Janai Kameka <jkameka@fb.com>
2020-07-13 16:34:41 -0400

    Added view controllers for the two new view controllers

---
+++ CalendarFinder/View Controllers/CalendarViewController.h
@@ -0,0 +1,17 @@
+//
+//  CalendarViewController.h
+//  CalendarFinder
+//
+//  Created by Janai Kameka on 7/13/20.
+//  Copyright © 2020 Janai Kameka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CalendarViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
