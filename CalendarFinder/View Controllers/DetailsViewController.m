9a5266643c2498d7fca746a7eb20e53eb81b0f14
Janai Kameka <jkameka@fb.com>
2020-07-22 09:52:20 -0400

    Details View shows start/end time

--- CalendarFinder/View Controllers/DetailsViewController.m
+++ CalendarFinder/View Controllers/DetailsViewController.m
@@ -11,9 +11,10 @@
 @interface DetailsViewController ()
 @property (weak, nonatomic) IBOutlet PFImageView *eventImageView;
 @property (weak, nonatomic) IBOutlet UILabel *eventNameLabel;
-@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *authorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *startDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *endDateLabel;
 
 
 @end
@@ -33,6 +34,12 @@
     self.eventImageView.file = self.event.image;
     [self.eventImageView loadInBackground];
     
+    
+    NSDateFormatter *formatter = [[NSDateFormatter  alloc] init];
+    formatter.dateFormat = @"E MMM d HH:mm";
+    formatter.dateStyle = NSDateFormatterShortStyle;
+    self.startDateLabel.text = [formatter stringFromDate:self.event.startTime];
+    self.endDateLabel.text = [formatter stringFromDate:self.event.endTime];
     
 }
 
