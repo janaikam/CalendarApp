5b25d920e3fcf19f488c3c6973057fbcd04313ee
Janai Kameka <jkameka@fb.com>
2020-08-05 16:43:42 -0400

    Added icon to login screen

---
+++ CalendarFinder/Assets.xcassets/Login Button - Facebook Login_files/LDq5Ejpd2ZD.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["7z9dw"]); }
+
+__d("IntlNub",["NubController"],function(a,b,c,d,e,f){a=function(a){"use strict";babelHelpers.inheritsLoose(b,a);function b(b){var c;c=a.call(this)||this;a.prototype.init.call(babelHelpers.assertThisInitialized(c),b);return c}return b}(b("NubController"));e.exports=a},null);
---
+++ CalendarFinder/Assets.xcassets/Login Button - Facebook Login_files/f6RzFBCuCAO.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["TMjYw"]); }
+
+__d("legacy:bug-js",["BugBar"],function(a,b,c,d,e,f){a.BugBar=a.BugBar||b("BugBar")},3);
